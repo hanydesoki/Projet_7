21a9b883d264b98cc989462634ba9ceb4e5a9625
Hany Desoki <desoki.hany@live.fr>
2021-12-23 13:07:54 +0100

    Avancement dashboard

--- P7_02_dashboard.py
+++ P7_02_dashboard.py
@@ -41,18 +41,32 @@ def load_data(folder, n_sample=None):
 
 application_df, bureau, bb, prev, pos, ins, cc = load_data('Datas', n_sample=10000)
 
-lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\
-    Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed,\
-        dolor. Cras elementum ultrices diam.\
-            Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit."
 
-ratio_target = application_df['TARGET'].mean()
+# %%
+
+def filter_df(df, col, classes):
+    return df[df[col].isin(classes)]
+
+# %%
+
+st.sidebar.header('Options')
+
+gender_option = st.sidebar.selectbox('Genre', ['Tout'] + list(application_df['CODE_GENDER'].unique()))
+
+
+# %%
+
+if gender_option == 'Tout':
+    data_filtered = application_df.copy()
+else:
+    data_filtered = filter_df(application_df, 'CODE_GENDER', [gender_option])
 
 st.header("Dashboard")
-st.subheader(f"Pourcentage de crédits refusés: {ratio_target*100} %")
 
 st.subheader('Applications')
 
-st.write(application_df)
+ratio_target = data_filtered['TARGET'].mean()
 
+st.subheader(f"Pourcentage de crédits refusés: {round(ratio_target * 100, 2)} %")
 
+st.write(data_filtered)

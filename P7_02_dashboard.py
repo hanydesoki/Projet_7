cb762d914beda29862f8d704077dd9e74983907f
Hany Desoki <desoki.hany@live.fr>
2021-12-29 13:13:34 +0100

    Avancement dashboard

--- P7_02_dashboard.py
+++ P7_02_dashboard.py
@@ -5,6 +5,7 @@ from custom_preprocessing import filter_data
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.graph_objects as go
 
 # %%
@@ -54,6 +55,7 @@ st.sidebar.header('Options')
 
 gender_option = st.sidebar.selectbox('Genre', ['Tout'] + list(application_df['CODE_GENDER'].unique()))
 target_option = st.sidebar.selectbox('Crédit', ['Tout', 'Accepté', 'Refusé'])
+car_option = st.sidebar.selectbox('Véhiculé', ['Tout', 'Oui', 'Non'])
 
 
 if gender_option == 'Tout':
@@ -66,6 +68,11 @@ if target_option == 'Accepté':
 elif target_option == 'Refusé':
     data_filtered = filter_df(application_df, 'TARGET', [1])
 
+if car_option == 'Oui':
+    data_filtered = filter_df(application_df, 'FLAG_OWN_CAR', ['Y'])
+elif car_option == 'Non':
+    data_filtered = filter_df(application_df, 'FLAG_OWN_CAR', ['N'])
+
 
 bureau, bb, prev, pos, ins, cc = filter_data(data_filtered, bureau, bb, prev, pos, ins, cc)
 
@@ -76,6 +83,17 @@ st.header("Dashboard")
 
 # %%
 st.subheader('Applications')
+
+data_filtered['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)
+# Some simple new features (percentages)
+data_filtered['DAYS_EMPLOYED_PERC'] = data_filtered['DAYS_EMPLOYED'] / data_filtered['DAYS_BIRTH']
+data_filtered['INCOME_CREDIT_PERC'] = data_filtered['AMT_INCOME_TOTAL'] / data_filtered['AMT_CREDIT']
+data_filtered['INCOME_PER_PERSON'] = data_filtered['AMT_INCOME_TOTAL'] / data_filtered['CNT_FAM_MEMBERS']
+data_filtered['ANNUITY_INCOME_PERC'] = data_filtered['AMT_ANNUITY'] / data_filtered['AMT_INCOME_TOTAL']
+data_filtered['PAYMENT_RATE'] = data_filtered['AMT_ANNUITY'] / data_filtered['AMT_CREDIT']
+
+new_features = ['DAYS_EMPLOYED_PERC', 'INCOME_CREDIT_PERC',
+                'ANNUITY_INCOME_PERC', 'PAYMENT_RATE']
 
 ratio_target = data_filtered['TARGET'].mean()
 
@@ -88,7 +106,7 @@ labels = data_filtered['NAME_CONTRACT_TYPE'].value_counts().index
 values = data_filtered['NAME_CONTRACT_TYPE'].value_counts().values
 
 #Contract type pie chart
-fig = go.Figure(
+fig1 = go.Figure(
     go.Pie(
     labels = labels,
     values = values,
@@ -97,7 +115,7 @@ fig = go.Figure(
 ))
 
 st.write("##### Type de contrat:")
-st.plotly_chart(fig)
+st.plotly_chart(fig1)
 
 # Amounts plot
 
@@ -105,12 +123,24 @@ x = ['AMT_INCOME_TOTAL', 'AMT_CREDIT', 'AMT_ANNUITY', 'AMT_GOODS_PRICE']
 
 y = data_filtered[x].mean(axis=0).values
 
-fig = go.Figure(go.Bar(x=x, y=y, text=values))
+fig2 = go.Figure(go.Bar(x=x, y=y, text=values))
 
-fig.update_layout(yaxis=dict(title='Moyenne ($)'))
+fig2.update_layout(yaxis=dict(title='Moyenne ($)'))
 
 st.write("##### Moyennes:")
-st.plotly_chart(fig)
+st.plotly_chart(fig2)
+
+# Rate plot plot
+
+y = data_filtered[new_features].mean(axis=0).values
+
+
+fig3 = go.Figure(go.Bar(x=new_features, y=y, text=values))
+
+fig3.update_layout(yaxis=dict(title='Ratio', range=[0,1]))
+
+st.write("##### Moyenne pourcentages:")
+st.plotly_chart(fig3)
 
 # %%
 

fbeb0d8d5dec6cd40d02add906618d6658b834b2
HANY DESOKI <88154293+sourisaboule@users.noreply.github.com>
2022-01-18 10:21:26 +0100

    Add files via upload

---
+++ custom_preprocessing.py
@@ -0,0 +1,279 @@
+import pandas as pd
+import numpy as np
+from data_science.preprocessing import DfEncoderOneHot
+from data_science.utils import ContextTimer
+from data_science.prexplo import describe_columns
+import gc
+
+class CustomPreprocessing:
+    bin_features = ['CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY']
+
+    def __init__(self, crit_missing_rate=None):
+        self.app_encoder = DfEncoderOneHot(nan_as_category=False)
+        self.bureau_encoder = DfEncoderOneHot(nan_as_category=True)
+        self.bb_encoder = DfEncoderOneHot(nan_as_category=True)
+        self.prev_encoder = DfEncoderOneHot(nan_as_category=True)
+        self.pos_encoder = DfEncoderOneHot(nan_as_category=True)
+        self.ins_encoder = DfEncoderOneHot(nan_as_category=True)
+        self.cc_encoder = DfEncoderOneHot(nan_as_category=True)
+
+        self.crit_missing_rate = crit_missing_rate
+
+        self.removed_columns = None
+
+        if self.crit_missing_rate is not None:
+            if (not isinstance(self.crit_missing_rate, int)) and (not isinstance(self.crit_missing_rate, float)):
+                raise TypeError(f'crit_missing_rate must be int or float or None, not a {self.crit_missing_rate.__class__.__name__}.')
+
+            if (self.crit_missing_rate < 0) or (self.crit_missing_rate > 1):
+                raise ValueError(f'crit_missing_rate must be between 0 and 1, got {self.crit_missing_rate}.')
+
+
+    def fit(self, application_df, bureau, bb, prev, pos, ins, cc):
+        self.app_encoder.fit(application_df)
+        self.bureau_encoder.fit(bureau)
+        self.bb_encoder.fit(bb)
+        self.prev_encoder.fit(prev)
+        self.pos_encoder.fit(pos)
+        self.ins_encoder.fit(ins)
+        self.cc_encoder.fit(cc)
+
+        self.removed_columns = None
+        
+    def transform(self, application_df, bureau, bb, prev, pos, ins, cc):
+        df = self.application_train_preprocessing(application_df)
+
+        with ContextTimer("Process bureau and bureau_balance"):
+            bureau = self.bureau_preprocessing(bureau, bb)
+            print("Bureau df shape:", bureau.shape)
+            df = df.join(bureau, how='left', on='SK_ID_CURR')
+            del bureau
+            gc.collect()
+        with ContextTimer("Process previous_applications"):
+            prev = self.previous_application_prerpocessing(prev)
+            print("Previous applications df shape:", prev.shape)
+            df = df.join(prev, how='left', on='SK_ID_CURR')
+            del prev
+            gc.collect()
+        with ContextTimer("Process POS-CASH balance"):
+            pos = self.pos_cash_preprocessing(pos)
+            print("Pos-cash balance df shape:", pos.shape)
+            df = df.join(pos, how='left', on='SK_ID_CURR')
+            del pos
+            gc.collect()
+        with ContextTimer("Process installments payments"):
+            ins = self.installments_payments_preprocessing(ins)
+            print("Installments payments df shape:", ins.shape)
+            df = df.join(ins, how='left', on='SK_ID_CURR')
+            del ins
+            gc.collect()
+        with ContextTimer("Process credit card balance"):
+            cc = self.credit_card_balance_preprocessing(cc)
+            print("Credit card balance df shape:", cc.shape)
+            df = df.join(cc, how='left', on='SK_ID_CURR')
+            del cc
+            gc.collect()
+
+        df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+        if self.crit_missing_rate is not None:
+            if self.removed_columns is None:
+                missing_rate_df = describe_columns(df)
+                self.removed_columns = list((missing_rate_df[missing_rate_df['NaN frequency'] > self.crit_missing_rate]).index)
+
+            if self.removed_columns:
+                df.drop(self.removed_columns, axis=1, inplace=True)
+
+
+        return df
+
+
+
+    def application_train_preprocessing(self, df):
+        df = self.app_encoder.transform(df)
+        df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)
+        # Some simple new features (percentages)
+        df['DAYS_EMPLOYED_PERC'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
+        df['INCOME_CREDIT_PERC'] = df['AMT_INCOME_TOTAL'] / df['AMT_CREDIT']
+        df['INCOME_PER_PERSON'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS']
+        df['ANNUITY_INCOME_PERC'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
+        df['PAYMENT_RATE'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
+        return df
+
+    def bureau_preprocessing(self, bureau, bb):
+        bureau = self.bureau_encoder.transform(bureau)
+        bb = self.bb_encoder.transform(bb)
+        bb_cat = self.bb_encoder.get_new_columns()
+        bureau_cat = self.bureau_encoder.get_new_columns()
+        # Bureau balance: Perform aggregations and merge with bureau.csv
+        bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size']}
+        for col in bb_cat:
+            bb_aggregations[col] = ['mean']
+        bb_agg = bb.groupby('SK_ID_BUREAU').agg(bb_aggregations)
+        bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in bb_agg.columns.tolist()])
+        bureau = bureau.join(bb_agg, how='left', on='SK_ID_BUREAU')
+        bureau.drop(['SK_ID_BUREAU'], axis=1, inplace=True)
+        del bb, bb_agg
+        gc.collect()
+
+        # Bureau and bureau_balance numeric features
+        num_aggregations = {
+            'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
+            'DAYS_CREDIT_ENDDATE': ['min', 'max', 'mean'],
+            'DAYS_CREDIT_UPDATE': ['mean'],
+            'CREDIT_DAY_OVERDUE': ['max', 'mean'],
+            'AMT_CREDIT_MAX_OVERDUE': ['mean'],
+            'AMT_CREDIT_SUM': ['max', 'mean', 'sum'],
+            'AMT_CREDIT_SUM_DEBT': ['max', 'mean', 'sum'],
+            'AMT_CREDIT_SUM_OVERDUE': ['mean'],
+            'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
+            'AMT_ANNUITY': ['max', 'mean'],
+            'CNT_CREDIT_PROLONG': ['sum'],
+            'MONTHS_BALANCE_MIN': ['min'],
+            'MONTHS_BALANCE_MAX': ['max'],
+            'MONTHS_BALANCE_SIZE': ['mean', 'sum']
+        }
+        # Bureau and bureau_balance categorical features
+        cat_aggregations = {}
+        for cat in bureau_cat: cat_aggregations[cat] = ['mean']
+        for cat in bb_cat: cat_aggregations[cat + "_MEAN"] = ['mean']
+
+        bureau_agg = bureau.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+        bureau_agg.columns = pd.Index(['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+        # Bureau: Active credits - using only numerical aggregations
+        active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
+        active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
+        active_agg.columns = pd.Index(['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
+        bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
+        del active, active_agg
+        gc.collect()
+        # Bureau: Closed credits - using only numerical aggregations
+        closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
+        closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
+        closed_agg.columns = pd.Index(['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
+        bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
+        del closed, closed_agg, bureau
+        gc.collect()
+        return bureau_agg
+
+    def previous_application_prerpocessing(self, prev):
+        prev = self.prev_encoder.transform(prev)
+        cat_cols = self.prev_encoder.get_new_columns()
+        # Days 365.243 values -> nan
+        prev['DAYS_FIRST_DRAWING'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_FIRST_DUE'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_LAST_DUE_1ST_VERSION'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_LAST_DUE'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_TERMINATION'].replace(365243, np.nan, inplace= True)
+        # Add feature: value ask / value received percentage
+        prev['APP_CREDIT_PERC'] = prev['AMT_APPLICATION'] / prev['AMT_CREDIT']
+        # Previous applications numeric features
+        num_aggregations = {
+            'AMT_ANNUITY': ['min', 'max', 'mean'],
+            'AMT_APPLICATION': ['min', 'max', 'mean'],
+            'AMT_CREDIT': ['min', 'max', 'mean'],
+            'APP_CREDIT_PERC': ['min', 'max', 'mean', 'var'],
+            'AMT_DOWN_PAYMENT': ['min', 'max', 'mean'],
+            'AMT_GOODS_PRICE': ['min', 'max', 'mean'],
+            'HOUR_APPR_PROCESS_START': ['min', 'max', 'mean'],
+            'RATE_DOWN_PAYMENT': ['min', 'max', 'mean'],
+            'DAYS_DECISION': ['min', 'max', 'mean'],
+            'CNT_PAYMENT': ['mean', 'sum'],
+        }
+        # Previous applications categorical features
+        cat_aggregations = {}
+        for cat in cat_cols:
+            cat_aggregations[cat] = ['mean']
+
+        prev_agg = prev.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+        prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+        # Previous Applications: Approved Applications - only numerical features
+        approved = prev[prev['NAME_CONTRACT_STATUS_Approved'] == 1]
+        approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
+        approved_agg.columns = pd.Index(['APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns.tolist()])
+        prev_agg = prev_agg.join(approved_agg, how='left', on='SK_ID_CURR')
+        # Previous Applications: Refused Applications - only numerical features
+        refused = prev[prev['NAME_CONTRACT_STATUS_Refused'] == 1]
+        refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
+        refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
+        prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
+        del refused, refused_agg, approved, approved_agg, prev
+        gc.collect()
+        return prev_agg
+
+    def pos_cash_preprocessing(self, pos):
+        pos = self.pos_encoder.transform(pos)
+        cat_cols = self.pos_encoder.get_new_columns()
+        # Features
+        aggregations = {
+            'MONTHS_BALANCE': ['max', 'mean', 'size'],
+            'SK_DPD': ['max', 'mean'],
+            'SK_DPD_DEF': ['max', 'mean']
+        }
+        for cat in cat_cols:
+            aggregations[cat] = ['mean']
+
+        pos_agg = pos.groupby('SK_ID_CURR').agg(aggregations)
+        pos_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in pos_agg.columns.tolist()])
+        # Count pos cash accounts
+        pos_agg['POS_COUNT'] = pos.groupby('SK_ID_CURR').size()
+        del pos
+        gc.collect()
+        return pos_agg
+
+    # Preprocess installments_payments.csv
+    def installments_payments_preprocessing(self, ins):
+        ins = self.ins_encoder.transform(ins)
+        cat_cols = self.ins_encoder.get_new_columns()
+        # Percentage and difference paid in each installment (amount paid and installment value)
+        ins['PAYMENT_PERC'] = ins['AMT_PAYMENT'] / ins['AMT_INSTALMENT']
+        ins['PAYMENT_DIFF'] = ins['AMT_INSTALMENT'] - ins['AMT_PAYMENT']
+        # Days past due and days before due (no negative values)
+        ins['DPD'] = ins['DAYS_ENTRY_PAYMENT'] - ins['DAYS_INSTALMENT']
+        ins['DBD'] = ins['DAYS_INSTALMENT'] - ins['DAYS_ENTRY_PAYMENT']
+        ins['DPD'] = ins['DPD'].apply(lambda x: x if x > 0 else 0)
+        ins['DBD'] = ins['DBD'].apply(lambda x: x if x > 0 else 0)
+        # Features: Perform aggregations
+        aggregations = {
+            'NUM_INSTALMENT_VERSION': ['nunique'],
+            'DPD': ['max', 'mean', 'sum'],
+            'DBD': ['max', 'mean', 'sum'],
+            'PAYMENT_PERC': ['max', 'mean', 'sum', 'var'],
+            'PAYMENT_DIFF': ['max', 'mean', 'sum', 'var'],
+            'AMT_INSTALMENT': ['max', 'mean', 'sum'],
+            'AMT_PAYMENT': ['min', 'max', 'mean', 'sum'],
+            'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum']
+        }
+        for cat in cat_cols:
+            aggregations[cat] = ['mean']
+        ins_agg = ins.groupby('SK_ID_CURR').agg(aggregations)
+        ins_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in ins_agg.columns.tolist()])
+        # Count installments accounts
+        ins_agg['INSTAL_COUNT'] = ins.groupby('SK_ID_CURR').size()
+        del ins
+        gc.collect()
+        return ins_agg
+
+    def credit_card_balance_preprocessing(self, cc):
+        cc = self.cc_encoder.transform(cc)
+        cc.drop(['SK_ID_PREV'], axis= 1, inplace = True)
+        cc_agg = cc.groupby('SK_ID_CURR').agg(['min', 'max', 'mean', 'sum', 'var'])
+        cc_agg.columns = pd.Index(['CC_' + e[0] + "_" + e[1].upper() for e in cc_agg.columns.tolist()])
+        # Count credit card lines
+        cc_agg['CC_COUNT'] = cc.groupby('SK_ID_CURR').size()
+        del cc
+        gc.collect()
+        return cc_agg
+
+
+def filter_data(application_df, bureau, bb, prev, pos, ins, cc):
+    skid_curr_filter = list(application_df['SK_ID_CURR'].unique())
+    bureau_filtered = bureau[bureau['SK_ID_CURR'].isin(skid_curr_filter)]
+    skid_bureau_filter = list(bureau_filtered['SK_ID_BUREAU'].unique())
+    bb_filtered = bb[bb['SK_ID_BUREAU'].isin(skid_bureau_filter)]
+    prev_filtered = prev[prev['SK_ID_CURR'].isin(skid_curr_filter)]
+    pos_filtered = pos[pos['SK_ID_CURR'].isin(skid_curr_filter)]
+    ins_filtered = ins[ins['SK_ID_CURR'].isin(skid_curr_filter)]
+    cc_filtered = cc[cc['SK_ID_CURR'].isin(skid_curr_filter)]
+
+    return bureau_filtered, bb_filtered, prev_filtered, pos_filtered, ins_filtered, cc_filtered
